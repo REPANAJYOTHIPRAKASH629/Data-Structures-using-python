defcf92204f38f23de89c4d2c5afac7ace1d9dbd
REPANAJYOTHIPRAKASH629 <98946604+REPANAJYOTHIPRAKASH629@users.noreply.github.com>
2022-12-21 10:41:02 +0530

    Add files via upload

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -1,90 +1,56 @@
-# A complete linked list implementation
-
-# Node Class
 class Node:
-    def __init__(self, data):
+    def __init__(self,data):
         self.data = data
         self.next = None
         self.prev = None
-
-
-# Linked List Class
 class DoublyLinkedList:
-    def __init__(self, initial_data=None):
+    def __init__(self,data=None,):
         self.head = None
         self.tail = None
         self.size = 0
-        if isinstance(initial_data, int):
-            self.insert_first(initial_data)
-        elif isinstance(initial_data, list):
-            for num in initial_data:
-                self.insert_last(num)
     def print(self):
         if self.head is None:
-            print('Doubly Linked list is empty')
+            print("Doubly linked list is empty.")
             return
         itr = self.head
-        llstr = ''
+        dllstr = ''
         while itr:
-            llstr=llstr+str(itr.data)+'<====>'
+            dllstr = dllstr + str(itr.data)+'<===>'
             itr = itr.next
-        print(llstr)
-
-
-    # Empty the linked list
-    def clear(self):
-        trav = self.head
-        while trav:
-            next = trav.next
-            trav.next = None
-            trav.prev = None
-            trav = next
-        self.head = self.tail = trav = None
-        self.size = 0
-
-    # Checks if the linked list is empty
+        print(dllstr)
     def is_empty(self):
         return self.size == 0
-
-    # Inserts at the begining
-    def insert_first(self, new_node):
+    def insert_at_beginning(self,new_node):
         new_node = Node(new_node)
         if self.is_empty():
             self.head = self.tail = new_node
-            self.size += 1
+            self.size+=1
             return
-
         new_node.next = self.head
         new_node.prev = None
         self.head = new_node
         self.head.next.prev = new_node
         self.size += 1
-
-    # Inserts at the end
-    def insert_last(self, new_node):
+    def insert_at_end(self,new_node):
         new_node = Node(new_node)
-
         if self.is_empty():
             self.head = self.tail = new_node
-            self.size += 1
+            self.size+=1
             return
-
         new_node.next = None
         new_node.prev = self.tail
         self.tail.next = new_node
         self.tail = new_node
         self.size += 1
-
-    # Inserts at a given position
-    def insert_at(self, at, new_elem):
+    def insert_at_position(self, at, new_elem):
         if at < 0 or at > self.size:
             raise Exception('Overflow: The given "at" postion is invalid')
 
         if at == 0:
-            self.insert_first(new_elem)
+            self.insert_at_beginning(new_elem)
             return
         if at == self.size:
-            self.insert_last(new_elem)
+            self.insert_at_end(new_elem)
             return
 
         index = 1
@@ -101,18 +67,13 @@ class DoublyLinkedList:
         curr_node.next = new_node
         next_node.prev = new_node
         self.size += 1
-
-    # Removes the first elem
     def remove_first(self):
         if self.is_empty():
             raise Exception("Nothing to remove")
-
         temp = self.head
         self.head = self.head.next
         self.size -= 1
         return temp.data
-
-    # Removes the last element
     def remove_last(self):
         if self.is_empty():
             raise Exception("Nothing to remove")
@@ -126,57 +87,54 @@ class DoublyLinkedList:
         if self.is_empty():
             self.head = None
         return temp.data
-
-    # Removes the first ocurrences of a piece of data
-    def remove(self, to_remove):
-        if self.is_empty():
-            raise Exception("Nothing to remove")
-
-        if self.head.data == to_remove:
-            return self.remove_first()
-        if self.tail.data == to_remove:
-            return self.remove_last()
-
-        curr_node = self.head
-        next_node = self.head.next
-
-        # Traverse until the node is found
-        while next_node:
-            if next_node.data == to_remove:
-                temp = next_node
-                next_node.next.prev = curr_node
-                curr_node.next = next_node.next
-                self.size -= 1
-                return temp.data
-            curr_node = curr_node.next
-            next_node = next_node.next
-        raise Exception("The element to remove was not found")
-
-    # Returns the index of a piece of data in the list, -1 otherwise
-    def index_of(self, data):
-        index = 0
-        last = self.head
-        while last:
-            if last.data == data:
-                return index
-            last = last.next
-            index += 1
-        return -1
-
-
-    # Len of linked list
-    def __len__(self):
-        return self.size
-
+    def remove_at_position(self, position):
+         if(position < 1):
+            print("\nposition should be >= 1.")
+         elif (position == 1 and self.head != None):
+             nodeToDelete = self.head
+             self.head = self.head.next
+             nodeToDelete = None
+             if (self.head != None):
+                          
+                 self.head.prev = None
+         else:
+             temp = self.head
+             for i in range(1, position-1):
+                 if(temp != None):
+                     temp = temp.next                        
+             if(temp != None and temp.next != None):
+                 nodeToDelete = temp.next
+                 temp.next = temp.next.next
+                 if(temp.next.next != None):
+                     temp.next.next.prev = temp.next  
+                     nodeToDelete = None 
+             else:
+                 print("\nThe node is already null.")
+    def get_length(self):
+        count=0
+        itr = self.head
+        while itr:
+            count+=1
+            itr = itr.next
+        return count
+ 
 if __name__=="__main__":
-     dll = DoublyLinkedList()
-     dll.insert_first([123])
-     dll.print()
-     dll.insert_last([456])
-     dll.print()
-     dll.insert_last([789])
-     dll.print()
-     dll.insert_at(2,[111213])
-     dll.print()
-     dll.remove([789])
-     dll.print()
+    dll = DoublyLinkedList()
+    dll.insert_at_beginning('akash')
+    dll.insert_at_beginning('prakash')
+    dll.insert_at_beginning('bubu')
+    dll.print()
+    dll.insert_at_end('mukesh')
+    dll.print()
+    dll.insert_at_position(3,'12345')
+    dll.print()
+    a = dll.get_length()
+    print(a)
+    dll.remove_at_position(3)
+    dll.print()
+    dll.remove_first()
+    dll.print()
+    dll.remove_last()
+    dll.print()
+    
+    
