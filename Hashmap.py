87c7fd385fc11887ebd92722870d2205dbb71e6f
REPANA JYOTHI PRAKASH <98946604+REPANAJYOTHIPRAKASH629@users.noreply.github.com>
2023-01-18 10:09:40 +0530

    Add files via upload

--- Hashmap.py
+++ Hashmap.py
@@ -1,65 +1,60 @@
 class HashTable:
     def __init__(self):
-        self.MAX=15
-        self.arr=[[] for i in range(self.MAX)]
+        self.MAX=10
+        self.arr=[0 for i in range(self.MAX)]
 
-    def gethash(self,key):
-        h=0
-        for char in key:
-            h+=ord(char)
-        return h % self.MAX
+    def get_hash(self,value):
+        h=value
+        return h%self.MAX
 
-    def setitem(self,key,val):
-        h=self.gethash(key)
-        for i in self.arr:
-            if (key,value) in i:
-                print("Inserted key, value already present")
-                break
-        self.arr[h].append((key,val))
-
-    def getitem(self,key):
-        h=self.gethash(key)
-        return self.arr[h]
-
-    def delitem(self,key,value):
-        h=self.gethash(key)
-        if len(self.arr[h])==1:
-            self.arr[h]=[]
+    def setitem(self,val):
+        h=self.get_hash(val)
+        if self.arr[h]==0:
+            self.arr[h]=val
         else:
-            for i in range(len(self.arr[h])):
-                if self.arr[h][i]==(key,value):
-                    self.arr[h].pop(i)
-                    break
+            #   Applying Linear Probabing
+            for i in range(1,self.MAX):
+                if self.arr[(h+i)%self.MAX]==0:
+                       self.arr[(h+i)%self.MAX]=val
+                       break
+            else:
+                print("Hash Table is full")
+
+
+    def delitem(self,val):
+        h=self.get_hash(val)
+        if self.arr[h]==val:
+            self.arr[h]=0
+        else:
+            for i in range(1,self.MAX):
+                if self.arr[(h+i)%self.MAX]==val:
+                       self.arr[(h+i)%self.MAX]=0
+                       break
 
     def display(self):
-        for i in self.arr:
-            print(i,end="")
-        print()
-        
+        print(self.arr)
+
+
 t=HashTable()
 
 while True:
-	ch=int(input("Enter your choice 1.Insert 2.Delete 3.Get value of key 4.Display 5.Exit"))
+	ch=int(input("Enter your choice 1.Insert 2.Delete 3.Display 4.Exit"))
 	
 	if ch==1:
-		print("Enter the key and value")
-		key=input()
+		print("Enter the value to insert")
 		value=int(input())
-		t.setitem(key,value)
+		t.setitem(value)
 	elif ch==2:
-		print("Enter the key and value to delete")
-		key=input()
+		print("Enter the value to delete")
 		value=int(input())
-		t. delitem(key,value)
+		t.delitem(value)
 	elif ch==3:
-		print("Enter key to know its value")
-		key=input()
-		print(t.getitem(key))
-	elif ch==4:
 		t.display()
-	elif ch==5:
+	elif ch==4:
 		break
 	else:
 	 	print("Invalid choice")
-		
-		
+
+
+
+    
